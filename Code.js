9536ac01adae5f9b4cc3047e147aa5900d2a929c
bsi-fujimoto-akinari <akinari.fujimoto@bsi-2010.co.jp>
2026-09-20 18:58:05 +0900

    Fix 5W audio folder target initialization (#82)
    * Fix written audio folder target initialization
    
    * Correct written audio folder target patch
    
    * Scope audio folder audit to Listening job

--- Code.js
+++ Code.js
@@ -5412,8 +5412,8 @@ function runJob_(
         folderId:
           audioTargetFolderId_(
             c,
-            '5L',
-            j.parentSetId
+            '5W',
+            j.id
           ),
         voices:
           q.concat(d),
